a1e3f6d5d40428fc7c36c0b8c822510ba0a3515a
atronics <arkadiusz@atronics.eu>
2020-07-12 15:49:49 +0200

    initial commit

---
+++ server/src/api/logs.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    message: '🌐',
+  });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+});
+
+module.exports = router;
---
+++ server/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const middleware = require('./middlewares');
+const logs = require('./api/logs');
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
+app.use('/api/logs', logs);
+
+app.use(middleware.notFound);
+app.use(middleware.errorHandler);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
